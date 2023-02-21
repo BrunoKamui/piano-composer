6b692c63f121fddf973bb44e20fc91ef78cc509d
Bruno Kamui <brunokamui.work@gmail.com>
2023-02-20 21:03:57 -0300

    feat: play key effect

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,17 @@ document.body.addEventListener('keyup', (event) => {
 
 function playSound(sound) {
   let audioElement = document.querySelector(`#sound-${sound}`);
+  let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
   if (audioElement) {
+    audioElement.currentTime = 0;
     audioElement.play();
+  }
+
+  if (keyElement) {
+    keyElement.classList.add('active');
+    setTimeout(() => {
+      keyElement.classList.remove('active');
+    }, 300);
   }
 }
